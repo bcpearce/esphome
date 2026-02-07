28b860caf9bd65833b718f2d6530662c373d93b5
bcpearce <admin@bcpearce.com>
2026-02-07 20:44:27 +0000

    Register as component

--- esphome/components/ti_lp5562/light.py
+++ esphome/components/ti_lp5562/light.py
@@ -11,20 +11,28 @@ TiLp5562Light = ti_lp5562_ns.class_(
     "TiLP5562LightOutput", light.LightOutput, i2c.I2CDevice
 )
 
-CONFIG_SCHEMA = light.RGB_LIGHT_SCHEMA.extend(
-    {
-        cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(TiLp5562Light),
-        cv.Optional(CONF_COLOR_MODE, default="RGB_WHITE"): cv.enum(
-            {k: COLOR_MODES[k] for k in ["RGB", "RGB_WHITE", "WHITE", "BRIGHTNESS"]},
-            upper=True,
-            space="_",
-        ),
-    }
-).extend(i2c.i2c_device_schema(0x30))
+CONFIG_SCHEMA = (
+    light.RGB_LIGHT_SCHEMA.extend(
+        {
+            cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(TiLp5562Light),
+            cv.Optional(CONF_COLOR_MODE, default="RGB_WHITE"): cv.enum(
+                {
+                    k: COLOR_MODES[k]
+                    for k in ["RGB", "RGB_WHITE", "WHITE", "BRIGHTNESS"]
+                },
+                upper=True,
+                space="_",
+            ),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(i2c.i2c_device_schema(0x30))
+)
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    await cg.register_component(var, config)
     await light.register_light(var, config)
     await i2c.register_i2c_device(var, config)
     cg.add(var.set_color_mode(config[CONF_COLOR_MODE]))
