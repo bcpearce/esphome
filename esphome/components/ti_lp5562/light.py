10f4d857e6c8ea4165f083f8c74f1a21ef1cce0b
bcpearce <admin@bcpearce.com>
2026-02-07 05:15:43 +0000

    Remove schema deps

--- esphome/components/ti_lp5562/light.py
+++ esphome/components/ti_lp5562/light.py
@@ -1,6 +1,5 @@
 import esphome.codegen as cg
 from esphome.components import i2c, light
-from esphome.components.rgbw import light as rgbw_light
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_OUTPUT_ID
 
@@ -9,9 +8,9 @@ DEPENDENCIES = ["i2c"]
 ti_lp5562_ns = cg.esphome_ns.namespace("ti_lp5562")
 TiLp5562Light = ti_lp5562_ns.class_("TiLp5562Light", i2c.I2CDevice)
 
-CONFIG_SCHEMA = rgbw_light.CONFIG_SCHEMA.extend(
+CONFIG_SCHEMA = i2c.i2c_device_schema(0x030).extend(
     {cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(TiLp5562Light)}
-).extend(i2c.i2c_device_schema(0x30))
+)
 
 
 async def to_code(config):
