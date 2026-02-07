42abd81bd54f53f3f4ba5e6eb999e239d9bb2677
bcpearce <admin@bcpearce.com>
2026-02-07 05:23:20 +0000

    OUTPUT

--- esphome/components/ti_lp5562/light.py
+++ esphome/components/ti_lp5562/light.py
@@ -2,7 +2,7 @@ import esphome.codegen as cg
 from esphome.components import i2c, light
 from esphome.components.rgbw.light import RGBWLightOutput
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_OUTPUT_ID
+from esphome.const import CONF_OUTPUT_ID
 
 DEPENDENCIES = ["i2c"]
 
@@ -15,6 +15,6 @@ CONFIG_SCHEMA = i2c.i2c_device_schema(0x030).extend(
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
     await light.register_light(var, config)
     await i2c.register_i2c_device(var, config)
